b4f809183ef0f561af73283f5ea1355f55f2f0d3
DanKoganov <152228621+DanKoganov@users.noreply.github.com>
2025-08-15 18:47:06 -0400

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,7 +2,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
-
+//LUXON CODE TEST
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
